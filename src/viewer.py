c920ef44b809170166ef1743122d2b95d064bb39
Ankit Shukla <shukla.ankit@protonmail.com>
2021-11-20 22:19:23 -0500

    changing label text

--- src/viewer.py
+++ src/viewer.py
@@ -12,9 +12,8 @@ class Viewer:
         self.pdf_reader = pdf_reader
         self.audio_manager = audio_manager
         self.page_no = 1
-        self.init_ui()
-        self.frm = ttk.Frame(self.root, padding=10)
         self.pdf_file_path = None
+        self.init_ui()
 
     def __del__(self):
         self.root.destroy()
@@ -23,22 +22,26 @@ class Viewer:
         self.root = Tk()
         self.root.title("My Audible")
         self.root.geometry("650x450+700+200")
-
-    def show_page(self): 
+        self.frm = ttk.Frame(self.root, padding=10)
         self.frm.grid()
         ttk.Button(self.frm, text ="Browse", command=self.browse_file).grid(row=0, column=0, padx=10, pady=10)
         ttk.Button(self.frm, text="Previous", command=self.prev_page).grid(row=0, column=1, padx=10, pady=10)
-        if self.pdf_file_path is not None and Path(self.pdf_file_path).is_file():
-            ttk.Label(self.frm, width=30, text="Page# " + str(self.page_no) + " / " + str(self.pdf_reader.page_count)).grid(row=0, column=2, padx=10, pady=10)
-        else:
-            ttk.Label(self.frm, width=30, text="Click Browse and select PDF file").grid(row=0, column=2, padx=10, pady=10)
+        self.labltext_page_no=StringVar(master=self.frm, value="Click Browse and select PDF file")
+        ttk.Label(self.frm, width=30, textvariable=self.labltext_page_no).grid(row=0, column=2, padx=10, pady=10)
         ttk.Button(self.frm, text="Next", command=self.next_page).grid(row=0, column=3, padx=10, pady=10)
+        #ttk.Button(self.frm, text="Exit", command=self.__del__()).grid(row=0, column=4, padx=10, pady=10)
+        self.labltext_page_content=StringVar(master=self.frm, value="")
+        ttk.Label(self.frm, textvariable=self.labltext_page_content).grid( row=1, padx= 100, pady=50)
+
+    def show_page(self): 
+        if self.pdf_file_path is not None and Path(self.pdf_file_path).is_file():
+            self.labltext_page_no.set("Page# " + str(self.page_no) + " / " + str(self.pdf_reader.page_count))
         if self.pdf_file_path is not None and Path(self.pdf_file_path).is_file():
             page_text = self.pdf_reader.read_page(self.page_no)
             if page_text is not None and len(page_text) > 0:
                 self.audio_manager.write(page_text, self.page_no)
                 self.audio_manager.play(self.page_no)
-                ttk.Label(self.frm, text=page_text).grid( row=1, padx= 100, pady=50)
+                self.labltext_page_content.set(page_text)
         self.root.mainloop()
 
     def browse_file(self):
